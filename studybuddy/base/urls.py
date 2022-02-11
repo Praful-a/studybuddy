f7defd21d893acc27c62881bce401d7b9eb62b7b
Praful-a <prafuldubey27@gmail.com>
2022-02-11 22:09:44 +0530

    created form-and-add-create-update-delete-functionality.

--- studybuddy/base/urls.py
+++ studybuddy/base/urls.py
@@ -1,11 +1,18 @@
-from django.urls import path 
+from django.urls import path
 from base.views import (
-		home,
-		room,
-	)
+    deleteRoom,
+    home,
+    room,
+    createRoom,
+    updateRoom,
+    deleteRoom,
+)
 
 
 urlpatterns = [
-	path('', home, name='home'),
-	path('room/', room, name="room"),
+    path('', home, name='home'),
+    path('room/<str:pk>/', room, name="room"),
+    path('create-room/', createRoom, name='create-room'),
+    path('update-room/<str:pk>/', updateRoom, name='update-room'),
+    path('delete-room/<str:pk>/', deleteRoom, name='delete-room'),
 ]
