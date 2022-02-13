5bcabb088e1002a06380df06a88d3095bedb5bfe
Praful-a <prafuldubey27@gmail.com>
2022-02-13 23:09:07 +0530

    creating-user-profile-page

--- studybuddy/base/urls.py
+++ studybuddy/base/urls.py
@@ -10,6 +10,7 @@ from base.views import (
     logoutUser,
     registerUser,
     deleteMessage,
+    userProfile
 )
 
 
@@ -19,6 +20,7 @@ urlpatterns = [
     path("logout/", logoutUser, name='logout'),
     path('', home, name='home'),
     path('room/<str:pk>/', room, name="room"),
+    path('profile/<str:pk>/', userProfile, name="user-profile"),
     path('create-room/', createRoom, name='create-room'),
     path('update-room/<str:pk>/', updateRoom, name='update-room'),
     path('delete-room/<str:pk>/', deleteRoom, name='delete-room'),
